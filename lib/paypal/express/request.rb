e2926579c22c403ed710ad80a7ab6883a142987e
boxmo <mztbrum@gmail.com>
2016-08-02 15:00:38 -0300

    more options to request

--- lib/paypal/express/request.rb
+++ lib/paypal/express/request.rb
@@ -25,7 +25,9 @@ module Paypal
           :locale        => :LOCALECODE,
           :logo          => :LOGOIMG,
           :cart_border_color => :CARTBORDERCOLOR,
-          :payflow_color => :PAYFLOWCOLOR
+          :payflow_color => :PAYFLOWCOLOR,
+          :ship_name => :SHIPTONAME,
+          :ship_phone => :SHIPTOPHONENUM
         }.each do |option_key, param_key|
           params[param_key] = options[option_key] if options[option_key]
         end
